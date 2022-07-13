fbfca9b15f91bce5c21814905298bfb10e284841
Alex Miller <alex@ajmillerdevelopment.com>
2022-07-12 22:21:41 -0700

    revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
-import SaveManager from "./components/SaveManager";
 
 function App() {
-    const [save, setSave] = useState(null);
-    return (
-        <div className="App">{!save && <SaveManager setSave={setSave} />}</div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
