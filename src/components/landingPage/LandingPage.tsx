81d3aa3cdf603663962a517caf680954a22be10c
Alex Miller <alex@ajmillerdevelopment.com>
2022-07-16 14:07:10 -0700

    landing page markup

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -2,32 +2,70 @@ import React from "react";
 
 export default function LandingPage() {
     return (
-        <div>
-            <header>
-                <h1 className="underline font-display">
+        <div className="flex page flex-col items-center">
+            <header className="mt-24 flex flex-col items-center">
+                <h1 className="underline font-display text-7xl">
                     HOI4 Production Manager
                 </h1>
-                <a href="https://github.com/ajmillerdevelopment/factory-manager">
+                <a
+                    className="mt-12 font-body"
+                    href="https://github.com/ajmillerdevelopment/factory-manager"
+                >
                     https://github.com/ajmillerdevelopment/factory-manager
                 </a>
             </header>
-            <main>
-                <div id="new-game-box">
-                    <h2>Create New Game File</h2>
-                    <p>Game Name</p>
+            <main className="w-full flex flex-col items-center">
+                <div
+                    id="new-game-box"
+                    className="bg-zinc-900 border-slate-500 flex flex-col items-center border w-6/12"
+                >
+                    <h2 className="font-display text-4xl text-center mt-6">
+                        Create New Game File
+                    </h2>
                     <input
                         type="text"
                         name="game-name"
                         placeholder="Your Game Name"
                         id="game-name-input"
+                        className="mt-2"
                     />
-                    <p>Choose your preset:</p>
-                    <div id="preset-buttons">
-                        <button id="vanilla-button">Vanilla</button>
-                        <button id="vanilla-button">Blank</button>
+                    <p className="mt-4 font-body">Choose your preset:</p>
+                    <div
+                        id="preset-buttons"
+                        className="flex flex-row w-full justify-center"
+                    >
+                        <button
+                            className="border border-slate-500 flex flex-col items-center w-4/12 m-4"
+                            id="vanilla-button"
+                        >
+                            <h3 className="font-display text-4xl mt-4">
+                                Vanilla
+                            </h3>
+                            <p className="font-body m-4">
+                                Start with vanilla values (you can still change
+                                these later)
+                            </p>
+                        </button>
+                        <button
+                            className="border border-slate-500 flex flex-col items-center w-4/12 m-4"
+                            id="vanilla-button"
+                        >
+                            <h3 className="font-display text-4xl mt-4">
+                                Blank
+                            </h3>
+                            <p className="font-body m-4">
+                                Start without any pre-set values (for use with
+                                overhaul mods)
+                            </p>
+                        </button>
                     </div>
                 </div>
-                <button id="load-game-button">Load Game File</button>
+                <button
+                    id="load-game-button"
+                    className="border border-slate-500 bg-zinc-900 flex flex-col items-center justify-center h-24 w-2/12 m-4"
+                >
+                    <h3 className="font-display text-2xl">Load Game File</h3>
+                </button>
             </main>
         </div>
     );
